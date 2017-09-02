7d28f0b135ca94e1d3e64e8f1a9e58d775154ee1
sfsquare <sfsquaretrust@gmail.com>
2017-09-02 17:02:05 +0530

    Adding Store Utility
    1) Added Store Utility
    2) Updated Logi Image
    3) Implemented Export to PDf in Profile page
    4) Fixed Image Save in the tables issue

--- SFSAcademy/SFSAcademy/Controllers/StudentController.cs
+++ SFSAcademy/SFSAcademy/Controllers/StudentController.cs
@@ -161,6 +161,149 @@ namespace SFSAcademy.Controllers
             return View(StudentS.FirstOrDefault());
         }
 
+
+
+        // GET: Student
+        [HttpGet]
+        public void ProfilePDF(int? id)
+        {
+
+            var StudentS = (from st in db.STUDENTs
+                            join b in db.BATCHes on st.BTCH_ID equals b.ID into gi
+                            from subb in gi.DefaultIfEmpty()
+                            join c in db.COURSEs on subb.CRS_ID equals c.ID into gj
+                            from subc in gj.DefaultIfEmpty()
+                            join ct in db.COUNTRies on st.NTLTY_ID equals ct.ID into gk
+                            from subct in gk.DefaultIfEmpty()
+                            join cat in db.STUDENT_CATGEORY on st.STDNT_CAT_ID equals cat.ID into gl
+                            from subcat in gl.DefaultIfEmpty()
+                            join emp in db.EMPLOYEEs on subb.EMP_ID equals emp.ID into gm
+                            from subemp in gm.DefaultIfEmpty()
+                            join img in db.IMAGE_DOCUMENTS on st.PHTO_DATA equals img.DocumentId into gn
+                            from subimg in gn.DefaultIfEmpty()
+                            where st.ID == id
+                            orderby st.LAST_NAME, subb.NAME
+                            select new Models.Student { StudentData = st, BatcheData = (subb == null ? null : subb), CourseData = (subc == null ? null : subc), CountryData = (subct == null ? null : subct), CategoryData = (subcat == null ? null : subcat), EmployeeData = (subemp == null ? null : subemp), ImageData = (subimg == null ? null : subimg) }).Distinct();
+
+
+            var PdfStudentS = (from res in StudentS
+                               select new { Photo= res.StudentData.PHTO_DATA,Name = string.Concat(res.StudentData.FIRST_NAME, " ", res.StudentData.MID_NAME, " ",res.StudentData.LAST_NAME), Cours = res.CourseData.CRS_NAME, Batch = res.BatcheData.NAME,AdDate = res.StudentData.ADMSN_DATE, AdNumber = res.StudentData.ADMSN_NO,DOB= res.StudentData.DOB, Bldg = res.StudentData.BLOOD_GRP, Gen= res.StudentData.GNDR,nation=res.CountryData.CTRY_NAME, lan=res.StudentData.LANG, cat = res.CategoryData.NAME,rel=res.StudentData.RLGN, Add= string.Concat(res.StudentData.ADDR_LINE1, " ", res.StudentData.ADDR_LINE2, " ", res.StudentData.CITY, " ", res.StudentData.STATE, " ", res.StudentData.PIN_CODE),ph = res.StudentData.PH1, mob = res.StudentData.PH2, grpt = string.Concat(res.EmployeeData.FIRST_NAME, " ", res.EmployeeData.LAST_NAME)}).ToList();
+
+            var configuration = new ReportConfiguration();
+            //configuration.PageOrientation = PageSize.LETTER_LANDSCAPE.Rotate();
+            configuration.LogoPath
+                = Server.MapPath(Url.Content("~/Content/images/login/SF_Square_Logo-Small.jpg"));
+            configuration.LogImageScalePercent = 50;
+            configuration.ReportTitle
+                = "S. F. Square Academy Student Profile";
+            configuration.ReportSubTitle = "Result of Stundet's Profile in School Database";
+
+            var report = new PdfTabularReport();
+            report.ReportConfiguration = configuration;
+
+            List<ReportColumn> columns = new List<ReportColumn>();
+            columns.Add(new ReportColumn { ColumnName = "Item", Width = 300 });
+            columns.Add(new ReportColumn { ColumnName = "Details", Width = 300 });
+
+            var PdfStudentSI = new DataTable();
+
+            PdfStudentSI.Columns.Add("Item", typeof(string));
+            PdfStudentSI.Columns.Add("Details", typeof(string));
+
+
+            var row2 = PdfStudentSI.NewRow();
+            row2["Item"] = "Name:";
+            row2["Details"] = PdfStudentS.FirstOrDefault().Name;
+            PdfStudentSI.Rows.Add(row2);
+
+            var row3 = PdfStudentSI.NewRow();
+            row3["Item"] = "Course:";
+            row3["Details"] = PdfStudentS.FirstOrDefault().Cours;
+            PdfStudentSI.Rows.Add(row3);
+
+            var row4 = PdfStudentSI.NewRow();
+            row4["Item"] = "Batch:";
+            row4["Details"] = PdfStudentS.FirstOrDefault().Batch;
+            PdfStudentSI.Rows.Add(row4);
+
+            var row5 = PdfStudentSI.NewRow();
+            row5["Item"] = "Admission Number:";
+            row5["Details"] = PdfStudentS.FirstOrDefault().AdNumber;
+            PdfStudentSI.Rows.Add(row5);
+
+            var row6 = PdfStudentSI.NewRow();
+            row6["Item"] = "Admission Date";
+            row6["Details"] = PdfStudentS.FirstOrDefault().AdDate;
+            PdfStudentSI.Rows.Add(row6);
+
+            var row7 = PdfStudentSI.NewRow();
+            row7["Item"] = "Date of Birth:";
+            row7["Details"] = PdfStudentS.FirstOrDefault().DOB;
+            PdfStudentSI.Rows.Add(row7);
+
+            var row8 = PdfStudentSI.NewRow();
+            row8["Item"] = "Blood Group:";
+            row8["Details"] = PdfStudentS.FirstOrDefault().Bldg;
+            PdfStudentSI.Rows.Add(row8);
+
+            var row9 = PdfStudentSI.NewRow();
+            row9["Item"] = "Gender:";
+            row9["Details"] = PdfStudentS.FirstOrDefault().Gen;
+            PdfStudentSI.Rows.Add(row9);
+
+            var row10 = PdfStudentSI.NewRow();
+            row10["Item"] = "Nationality:";
+            row10["Details"] = PdfStudentS.FirstOrDefault().nation;
+            PdfStudentSI.Rows.Add(row10);
+
+            var row11 = PdfStudentSI.NewRow();
+            row11["Item"] = "Language:";
+            row11["Details"] = PdfStudentS.FirstOrDefault().lan;
+            PdfStudentSI.Rows.Add(row11);
+
+            var row12 = PdfStudentSI.NewRow();
+            row12["Item"] = "Category:";
+            row12["Details"] = PdfStudentS.FirstOrDefault().cat;
+            PdfStudentSI.Rows.Add(row12);
+
+            var row13 = PdfStudentSI.NewRow();
+            row13["Item"] = "Religion:";
+            row13["Details"] = PdfStudentS.FirstOrDefault().rel;
+            PdfStudentSI.Rows.Add(row13);
+
+            var row14 = PdfStudentSI.NewRow();
+            row14["Item"] = "Address:";
+            row14["Details"] = PdfStudentS.FirstOrDefault().Add;
+            PdfStudentSI.Rows.Add(row14);
+
+            var row15 = PdfStudentSI.NewRow();
+            row15["Item"] = "Phone:";
+            row15["Details"] = PdfStudentS.FirstOrDefault().ph;
+            PdfStudentSI.Rows.Add(row15);
+
+            var row16 = PdfStudentSI.NewRow();
+            row16["Item"] = "Mobile:";
+            row16["Details"] = PdfStudentS.FirstOrDefault().mob;
+            PdfStudentSI.Rows.Add(row16);
+
+            var row17 = PdfStudentSI.NewRow();
+            row17["Item"] = "Group Tutor:";
+            row17["Details"] = PdfStudentS.FirstOrDefault().grpt;
+            PdfStudentSI.Rows.Add(row17);
+
+
+            var stream = report.GetPdf(PdfStudentSI, columns);
+
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition",
+                "attachment;filename=ExampleReport.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.BinaryWrite(stream.ToArray());
+            Response.End();
+
+        }
+
         // GET: Student
         public ActionResult AdvancedSearch(string sortOrder, string currentFilter, string searchString, int? page, string currentFilter2, string AdmissionNumber, string currentFilter3, string Course, string currentFilter4, string CourseBatches, string currentFilter5, string Category, string currentFilter6, string StudentGender, string currentFilter7, string BloodGroup, string currentFilter8, string StudentGrade, string currentFilter9, string StudentBirthFromDate, string currentFilter10, string StudentBirthToDate)
         {
@@ -346,7 +489,7 @@ namespace SFSAcademy.Controllers
             }
             else { StudentBirthToDate = currentFilter10; }
             DateTime? dTo; DateTime dtTo;
-            dTo = DateTime.TryParse(StudentBirthFromDate, out dtTo) ? dtTo : (DateTime?)null;
+            dTo = DateTime.TryParse(StudentBirthToDate, out dtTo) ? dtTo : (DateTime?)null;
             ViewBag.CurrentFilter10 = StudentBirthToDate;
 
             var StudentS = (from st in db.STUDENTs
@@ -519,7 +662,7 @@ namespace SFSAcademy.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Admission1([Bind(Include = "ID,ADMSN_NO,CLS_ROLL_NO,ADMSN_DATE,FIRST_NAME,MID_NAME,LAST_NAME,BTCH_ID,DOB,GNDR,BLOOD_GRP,BIRTH_PLACE,LANG,RLGN,ADDR_LINE1,ADDR_LINE2,CITY,STATE,PIN_CODE,CTRY_ID,PH1,PH2,EML,IMMDT_CNTCT_ID,IS_SMS_ENABL,PHTO_FILENAME,PHTO_CNTNT_TYPE,PHTO_DATA,STAT_DESCR,IS_ACT,IS_DEL,CREATED_AT,UPDATED_AT,HAS_PD_FE,PHTO_FILE_SIZE,USRID,STDNT_CAT_ID,NTLTY_ID")] STUDENT sTUDENT, HttpPostedFileBase[] PHTO_FILENAME, string selectedTags)
+        public ActionResult Admission1([Bind(Include = "ID,ADMSN_NO,CLS_ROLL_NO,ADMSN_DATE,FIRST_NAME,MID_NAME,LAST_NAME,BTCH_ID,DOB,GNDR,BLOOD_GRP,BIRTH_PLACE,LANG,RLGN,ADDR_LINE1,ADDR_LINE2,CITY,STATE,PIN_CODE,CTRY_ID,PH1,PH2,EML,IMMDT_CNTCT_ID,IS_SMS_ENABL,PHTO_FILENAME,PHTO_CNTNT_TYPE,PHTO_DATA,STAT_DESCR,IS_ACT,IS_DEL,CREATED_AT,UPDATED_AT,HAS_PD_FE,PHTO_FILE_SIZE,USRID,STDNT_CAT_ID,NTLTY_ID")] STUDENT sTUDENT)
         {
             int UserId = Convert.ToInt32(this.Session["UserId"]);
             sTUDENT.USRID = UserId;
@@ -528,18 +671,16 @@ namespace SFSAcademy.Controllers
                 /////Picture Upload Code
                 string FileName = null;
                 SuccessModel viewModel = new SuccessModel();
-                if (Request.Files.Count == 1)
+                if (Request.Files.Count == 1 && Request.Files[0].FileName != "")
                 {
                     var name = Request.Files[0].FileName;
                     var size = Request.Files[0].ContentLength;
                     var type = Request.Files[0].ContentType;
                     FileName = name;
-                    viewModel.Success = HandleUpload(Request.Files[0].InputStream, name, size, type);
-                    var PicDocumentId = (from C in db.IMAGE_DOCUMENTS
-                                         where C.Name == FileName
-                                         select C.DocumentId).FirstOrDefault().ToString();
+                    int PhotoId = 0;
+                    PhotoId = HandleUpload(Request.Files[0].InputStream, name, size, type, PhotoId);
 
-                    sTUDENT.PHTO_DATA = Convert.ToInt32(PicDocumentId);
+                    sTUDENT.PHTO_DATA = PhotoId;
                     sTUDENT.PHTO_FILENAME = null;
                     sTUDENT.PHTO_CNTNT_TYPE = null;
                 }
@@ -799,24 +940,14 @@ namespace SFSAcademy.Controllers
                 /////Picture Upload Code
                 string FileName = null;
                 SuccessModel viewModel = new SuccessModel();
-                if (Request.Files.Count == 1)
+                if (Request.Files.Count == 1 && Request.Files[0].FileName != "")
                 {
                     var name = Request.Files[0].FileName;
                     var size = Request.Files[0].ContentLength;
                     var type = Request.Files[0].ContentType;
                     FileName = name;
-                    viewModel.Success = HandleUpload(Request.Files[0].InputStream, name, size, type);
-                    var PicDocumentId = (from C in db.IMAGE_DOCUMENTS
-                                         where C.Name == FileName
-                                         select C.DocumentId).FirstOrDefault().ToString();
+                    sTUDENT.PHTO_DATA = HandleUpload(Request.Files[0].InputStream, name, size, type, Convert.ToInt32(sTUDENT.PHTO_DATA));
 
-                    sTUDENT.PHTO_DATA = Convert.ToInt32(PicDocumentId);
-                    sTUDENT.PHTO_FILENAME = null;
-                    sTUDENT.PHTO_CNTNT_TYPE = null;
-                }
-                else
-                {
-                    sTUDENT.PHTO_DATA = 1;
                     sTUDENT.PHTO_FILENAME = null;
                     sTUDENT.PHTO_CNTNT_TYPE = null;
                 }
@@ -985,12 +1116,14 @@ namespace SFSAcademy.Controllers
                 var name = Request.Files[0].FileName;
                 var size = Request.Files[0].ContentLength;
                 var type = Request.Files[0].ContentType;
-                viewModel.Success = HandleUpload(Request.Files[0].InputStream, name, size, type);
+                int PhotoId = 0;
+                //viewModel.Success = HandleUpload(Request.Files[0].InputStream, name, size, type, PhotoId);
+                PhotoId = HandleUpload(Request.Files[0].InputStream, name, size, type, PhotoId);
             }
             return Json(viewModel);
         }
 
-        private bool HandleUpload(Stream fileStream, string name, int size, string type)
+        private int HandleUpload(Stream fileStream, string name, int size, string type, int PhotoId)
         {
             bool handled = false;
 
@@ -1001,26 +1134,42 @@ namespace SFSAcademy.Controllers
                 byte[] documentBytes = new byte[imageBufferedStream.Length];
                 imageBufferedStream.Read(documentBytes, 0, documentBytes.Length);
 
-
-                IMAGE_DOCUMENTS databaseDocument = new IMAGE_DOCUMENTS
+                if (PhotoId==0)
                 {
-                    CreatedOn = DateTime.Now,
-                    FileContent = documentBytes,
-                    IsDeleted = false,
-                    Name = name,
-                    Size = size,
-                    Type = type
-                };
+                    IMAGE_DOCUMENTS databaseDocument = new IMAGE_DOCUMENTS
+                    {
+                        CreatedOn = DateTime.Now,
+                        FileContent = documentBytes,
+                        IsDeleted = false,
+                        Name = name,
+                        Size = size,
+                        Type = type
+                    };
 
-                db.IMAGE_DOCUMENTS.Add(databaseDocument);
-                handled = (db.SaveChanges() > 0);
+                    db.IMAGE_DOCUMENTS.Add(databaseDocument);   
+                    handled = (db.SaveChanges() > 0);
+                    PhotoId = databaseDocument.DocumentId;
+                }
+                else
+                {
+                    IMAGE_DOCUMENTS ImagetoUpdate = db.IMAGE_DOCUMENTS.Find(PhotoId);
+                    ImagetoUpdate.CreatedOn = DateTime.Now;
+                    ImagetoUpdate.FileContent = documentBytes;
+                    ImagetoUpdate.Name = name;
+                    ImagetoUpdate.Size = size;
+                    ImagetoUpdate.Type = type;
+
+                    db.Entry(ImagetoUpdate).State = EntityState.Modified;
+                    handled = (db.SaveChanges() > 0);
+                }
+                
             }
             catch (Exception ex)
             {
                 throw ex; // Oops, something went wrong, handle the exception
             }
 
-            return handled;
+            return PhotoId;
         }
 
         private byte[] LoadImage(int id, out string type)
---
+++ SFSAcademy/SFSAcademy/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SFSAcademy.Models
+{
+    public class Products
+    {
+        public STORE_PRODUCTS ProductData { get; set; }
+        public STORE_CATEGORY CategoryData { get; set; }
+    }
+    public class Purchase
+    {
+        public STORE_PURCHAGE PurchaseData { get; set; }
+        public STORE_PRODUCTS ProductData { get; set; }
+        public STORE_CATEGORY CategoryData { get; set; }
+    }
+}
